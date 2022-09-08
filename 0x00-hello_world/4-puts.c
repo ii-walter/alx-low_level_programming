e29f338ec05e8dc312179df6f501fae9d457d83c
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-08 15:24:36 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *Description: main - Prints a string to stdout.
+ *Return: 0 if success.
+ **/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
