b03ecc5c7a81d228fd73c3d333d11749e316f27a
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-12 15:30:18 -0700

    8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * code below
+	 * lower alphabet first
+	 * uppercase next
+	 */
+	int n;
+	int x;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
+
+}
