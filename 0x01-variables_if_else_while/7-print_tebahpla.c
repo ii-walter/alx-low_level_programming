07edadb19d1cc50e457af36967e163ae4d7efb24
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-12 15:26:20 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * code below
+	 * lower alphabet first
+	 * uppercase next
+	 */
+	int n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+
+
+	return (0);
+
+}
