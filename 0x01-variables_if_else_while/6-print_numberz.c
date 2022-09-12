0e079d996494d8ea73a4b53e5ad52c42df4246e2
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-12 15:15:23 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * code below
+	 * lower alphabet first
+	 * uppercase next
+	 */
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	{
+		putchar('\n');
+	}
+
+	return (0);
+
+}
