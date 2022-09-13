843878ac0922426efd37ea2f89d1fc6f0b32b7b4
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-13 16:29:08 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-
 	int n = '0';
 
 	for (n = '0'; n <= '9'; n++)
@@ -15,7 +14,7 @@ int main(void)
 		putchar((n %  10) + '0');
 	
 		if (n == 9)
-			continue;
+		continue;
 		putchar(',');
 		putchar(' ');
 	}
