7561429fc288428681348323e789c6f984146c87
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-13 19:13:21 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * add - check the code
+ * add two integers and return the value
+ * @c: number
+ * @d: number
+ *
+ * Return: Always 0.
+ */
+
+int add(int c, int d)
+{
+	int add(int c, int d);
+	return (c + d);
+}
