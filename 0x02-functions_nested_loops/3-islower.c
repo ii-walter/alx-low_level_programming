edd2059bc3462929997c85c3e5b8156be36270c0
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-13 17:53:30 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - check the code
+ * @c: is letter required
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
