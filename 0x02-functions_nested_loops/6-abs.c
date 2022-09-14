a46be5bfa8114094272f8dd32d07d8f1e746c221
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-13 18:45:30 -0700

    6. No absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * check for an integer
+ *
+ * @n: is letter required
+ *
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = - n;
+	}
+	return (n);
+}
