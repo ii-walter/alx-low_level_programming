306a24dfb55f86d3ffa61480ab0a788299a5dadc
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-13 17:41:47 -0700

    1.Insomnia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int x = 97;
+
+	while (x <= 122)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
