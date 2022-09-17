97583e56ccb69e9abdf8a173099cfbaec56f35cf
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-17 05:06:03 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @sixe: size of the triangle..
+ *
+ * Return: Always 0 (Success)
+ */
+void print_triangle(int size)
+{
+	int x;
+	int y;
+	int z;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 1; x <= size; x++)
+		{
+			for (y = size - x; y > 0; y--)
+			{
+				_putchar(' ');
+			}
+		for (z = 0; z < x; z++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+}
