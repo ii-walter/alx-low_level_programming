1d7c41191f3526c448d2fbdbcca747247297588a
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-17 03:29:55 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints number between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int z = 48;
+
+	for (z = 48; z < 58; z++)
+	{
+		_putchar(z);
+	}
+	_putchar('\n');
+}
