bdb356b96b9da9c05aa9e1c4bff2aa757e4285da
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-17 04:07:47 -0700

    7. I feel like i am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal line
+ * @n; number of times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+
+	int a, b, c;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (a = 0; a < n; a++)
+		{
+			c = a - 1;
+
+			for (b = 0; b <= c; b++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
