07761d4864596410a3984e12424b350843da5f1e
Walter Ibekwe <walterifeanyichukwy@gmail.com>
2022-09-17 05:16:28 -0700

    11. Prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int x = 612852475143;
+	long int p;
+
+	for (p = 2; p < x; p++)
+	{
+		if (x % p == 0)
+		{
+			x = x / p;
+		}
+	}
+	printf("%ld\n", p);
+	return (0);
+}
